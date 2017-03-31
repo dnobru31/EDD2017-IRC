d70e4515eea7abbe8a6235ee6aafaa872548cfda
Bruno <bruno.demblans@berger-levrault.fr>
2017-03-31 15:28:13 +0200

    BDEM: archivage des messages [archive()] dans Salon + tests unitaires

---
+++ Tests/com/cfranc/irc/server/SalonTest.java
@@ -0,0 +1,30 @@
+package com.cfranc.irc.server;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SalonTest {
+
+	@Test
+	public void archiveTest() { // la taille max du tableau (tailleMaxHisto) est à 3 en dur (classe Salon)
+		Salon actuel = new Salon("actuel", false);
+		assertEquals (0, actuel.histoSalon.size()); // on teste la taille du tableau a chaque ajout de message
+		actuel.archive("1er message");
+		assertEquals (1, actuel.histoSalon.size());
+		actuel.archive("2ème message");
+		assertEquals (2, actuel.histoSalon.size());
+		actuel.archive("3ème message");
+		assertEquals (3, actuel.histoSalon.size());
+		actuel.archive("4ème message");
+		assertEquals (3, actuel.histoSalon.size());
+		
+		// on teste qu'à l'ajout du 4ème message le 2ème message soit passé de la position 1 à la position 0
+		assertEquals("[2ème message, 3ème message, 4ème message]", actuel.histoSalon.toString());
+
+		
+		
+		//System.out.println(actuel.histoSalon);
+	}
+
+}
--- src/com/cfranc/irc/server/Salon.java
+++ src/com/cfranc/irc/server/Salon.java
@@ -1,6 +1,7 @@
 package com.cfranc.irc.server;
 
 import java.util.List;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 // Pour gérer les salons, a priori, le mode salon privé permettrait
 // de gérer une communication privée entre deux utilisateur
@@ -11,6 +12,8 @@ public class Salon {
 	private boolean bPrivate=false;
 	
 	private List<User> listeDesUsers;
+	
+	public ConcurrentLinkedQueue<String> histoSalon;
 	
 	public String getNomSalon() {
 		return nomSalon;
@@ -28,11 +31,23 @@ public class Salon {
 		super();
 		this.nomSalon = nomSalon;
 		this.bPrivate = bPrivate;
+		this.histoSalon = new ConcurrentLinkedQueue<String>();
 	}
 	
 	//User userCreator = null; need it ?
 	
-	public void archive (String mess)  {
+
+	// si la liste est < à la taille max, rajouter newMess à la fin; sinon supprimer le 1er message de la liste et rajouter newMessage à la fin
+	// la liste (ConcurrentLinkedQueue<String>) est créée par le constructeur de Salon
+	public void archive (String newMess)  {
+		int tailleMaxHisto = 3;
+		
+		if (histoSalon.size()< tailleMaxHisto) {
+			histoSalon.add(newMess);
+		}else{
+			histoSalon.remove();
+			histoSalon.add(newMess);
+		}
 		
 	}
 
